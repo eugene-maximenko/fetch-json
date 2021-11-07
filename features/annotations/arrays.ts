f62250356da13a23ad59667344df35f2f1e3b10f
ShadowPrince <zhek4770@gmail.com>
2021-11-07 20:57:48 +0200

    Explore arrays

--- features/annotations/arrays.ts
+++ features/annotations/arrays.ts
@@ -6,3 +6,21 @@ const carsByMake: string[][] = [
     ['corolla'],
     ['camaro']
 ]
+
+// Help with inference when exctracting values
+const car = carMakers[0]
+const myCar = carMakers.pop()
+
+// Prevent incompatible values
+carMakers.push('100')
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+    return car.toUpperCase()
+})
+
+// Flexible types
+const importantDates: (Date | string)[] = [new Date()]
+importantDates.push('2030-10-10')
+importantDates.push(new Date())
+
