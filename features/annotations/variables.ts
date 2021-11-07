b41cde2f82788c6355c099d38abdad0701a0afaf
ShadowPrince <zhek4770@gmail.com>
2021-11-07 17:50:50 +0200

    Explored case with declaration and initialization on separate lines

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -34,5 +34,17 @@ const logNumber: (i: number) => void = (i: number) => {
 // When to use annotations
 // 1) Function that returns the 'any' type
 const json = '{"x":10,"y": 20}'
-const coordinates = JSON.parse(json)
+const coordinates: {x: number; y: number} = JSON.parse(json)
 console.log(coordinates)
+
+// 2) When we declare a variable on one line
+// and initialize it later
+let words = ['red', 'green', 'blue']
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i] === 'green') {
+        foundWord === true
+    }
+}
+
