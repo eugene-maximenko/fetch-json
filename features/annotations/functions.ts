1516e3a416af161ef87a96d607847ed9e11942ac
ShadowPrince <zhek4770@gmail.com>
2021-11-07 19:44:43 +0200

    Explored objects destructuring

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -24,3 +24,15 @@ const throwError = (message: string): void => {
         throw new Error(message);
     }
 }
+
+const todaysWeather = {
+    date: new Date(),
+    weather: 'sunny'
+}
+
+const logWeather = ({date, weather}: {date: Date, weather: string}): void => {
+    console.log(date)
+    console.log(weather)
+}
+
+logWeather(todaysWeather)
