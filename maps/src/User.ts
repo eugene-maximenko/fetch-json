fe25a745cdf2f2ef8003fbd45e7072b64aa4f8e1
ShadowPrince <zhek4770@gmail.com>
2021-11-10 00:20:14 +0200

    Initialize fields with faker

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -8,6 +8,10 @@ class User {
     }
 
     constructor () {
-
+        this.name = faker.name.findName()
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        }
     }
 }
